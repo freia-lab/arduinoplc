218c6fc138c97b4348b4df68d801211e59d2ab1e
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-07-30 17:55:19 +0200

    Possible to set 1 coil but no reply from TCP server.

---
+++ arduinoplc/O.7.0.7_Common/arduinoplc.db.d
@@ -0,0 +1,2 @@
+O.7.0.7_Common/arduinoplc.db: ./Db/arduinoplc-bo.template \
+ ./Db/arduinoplc-longin.template
--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,3 +1,16 @@
 # This should be a test or example startup script
 
 require arduinoplc
+require recsync
+require iocstats
+
+
+iocshLoad ("$(arduinoplc_DIR)arduinoplc.iocsh", "ASYN_PORT_NAME=PORT1,IP_ADDR=192.168.10.40,TOP=/opt/epics/autosave,IOCNAME=ioc24-arduinoplc,PREFIX=test:,DEV_NAME=Coils:")
+
+
+# iocStats database
+#dbLoadRecords("$(iocstats_DB)/iocAdminSoft-freia.db","IOC=ioc22-siargo")
+
+# Start recsync client
+# iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=ioc24-arduinoplc")
+
