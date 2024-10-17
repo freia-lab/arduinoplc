e3451b7fabfe8de0f57d5aba32f8b459dbb6940d
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-10-17 14:17:24 +0200

    Removed all stuff not directly connected with arduino plc.

--- cmds/cav-selector.cmd
+++
@@ -1,8 +0,0 @@
-require arduinoplc
-require afterinit
-require autosave
-
-
-epicsEnvSet("IOCDIR", "cav-selector")
-dbLoadRecords("cav-selector.db", "P=LLRF-Ctrl:")
-iocshLoad("$(autosave_DIR)/autosave.iocsh", "AS_TOP=/tmp,IOCNAME=cav-selector")
